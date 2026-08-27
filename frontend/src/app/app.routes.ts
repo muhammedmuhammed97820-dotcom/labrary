113e68009fb31e48b0872d48feda755ed10663ec
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-08-27 18:39:21 +0300

    Add authentication routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,5 +5,7 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'books', loadComponent: () => import('./features/books/books.component').then(m => m.BooksComponent) },
   { path: 'books/:id', loadComponent: () => import('./features/books/book-details/book-details.component').then(m => m.BookDetailsComponent) },
+  { path: 'login', loadComponent: () => import('./features/auth/auth.component').then(m => m.AuthComponent) },
+  { path: 'register', loadComponent: () => import('./features/auth/auth.component').then(m => m.AuthComponent) },
   { path: '**', redirectTo: '' }
 ];
