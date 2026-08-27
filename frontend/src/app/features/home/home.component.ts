ac01b082ae4ffa0ce292d22b714e4719134f8c43
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-08-27 22:36:32 +0300

    Move home styling to global stylesheet

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,11 +1,4 @@
-﻿import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
-})
+@Component({selector:'app-home',standalone:true,imports:[RouterLink],encapsulation:ViewEncapsulation.None,templateUrl:'./home.component.html'})
 export class HomeComponent {}
