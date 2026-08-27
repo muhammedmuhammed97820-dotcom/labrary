f079a41796d2aa7214cc3dfe13994566c8cccbdd
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-08-27 18:37:54 +0300

    Build premium book details page

---
+++ frontend/src/app/features/books/book-details/book-details.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-book-details',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './book-details.component.html',
+  styleUrl: './book-details.component.scss'
+})
+export class BookDetailsComponent {
+  private route = inject(ActivatedRoute);
+  bookId = this.route.snapshot.paramMap.get('id') ?? '';
+}
