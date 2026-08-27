f4f4fe88b330d4e2410825ca566d379227829242
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-08-27 19:20:52 +0300

    Register JWT auth interceptor globally

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideHttpClient(withFetch()), provideRouter(routes)]
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideRouter(routes)
+  ]
 };
