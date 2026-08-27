bfb49716fdbf34e19755b11ff3aca7f385ecd4b1
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-08-27 17:55:34 +0300

    Integrate shared navbar into app shell

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,24 +1,12 @@
-﻿import {
-  Component
-} from '@angular/core';
-
-import {
-  RouterOutlet
-} from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-
   standalone: true,
-
-  imports: [
-    RouterOutlet
-  ],
-
-  templateUrl:
-    './app.html',
-
-  styleUrl:
-    './app.scss'
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
 export class App {}
