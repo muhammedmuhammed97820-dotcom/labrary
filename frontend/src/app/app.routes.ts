0e6fbbb410de65bd01b44a87826c119c92cf042f
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-08-27 18:38:11 +0300

    Add book details route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,19 +1,9 @@
-﻿import { Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'books',
-    loadComponent: () =>
-      import('./features/books/books.component')
-        .then(m => m.BooksComponent)
-  },
-  {
-    path: '**',
-    redirectTo: ''
-  }
+  { path: '', component: HomeComponent },
+  { path: 'books', loadComponent: () => import('./features/books/books.component').then(m => m.BooksComponent) },
+  { path: 'books/:id', loadComponent: () => import('./features/books/book-details/book-details.component').then(m => m.BookDetailsComponent) },
+  { path: '**', redirectTo: '' }
 ];
