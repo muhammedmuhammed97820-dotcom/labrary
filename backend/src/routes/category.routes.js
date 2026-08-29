73e91bcb0252dff99418afbf0b7f4a7d15858214
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-08-29 20:57:35 +0300

    feat(categories): add category detail route

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,19 +1,9 @@
-﻿const express = require("express");
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/category.controller");
 
-const router =
-  express.Router();
-
-const controller =
-  require("../controllers/category.controller");
-
-router.get(
-  "/",
-  controller.getCategories
-);
-
-router.post(
-  "/",
-  controller.createCategory
-);
+router.get("/", controller.getCategories);
+router.get("/:id", controller.getCategory);
+router.post("/", controller.createCategory);
 
 module.exports = router;
