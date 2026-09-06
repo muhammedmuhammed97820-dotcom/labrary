c235066a994178a3eb3fbc52df168b26d7da7f26
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-09-06 10:19:02 +0300

    Add GridFS author image endpoint

--- backend/src/routes/author.routes.js
+++ backend/src/routes/author.routes.js
@@ -5,6 +5,7 @@ const controller = require("../controllers/author.controller");
 const { authenticate, requireAdmin } = require("../middleware/auth.middleware");
 
 router.get("/", controller.getAuthors);
+router.get("/:id/image", controller.streamAuthorImage);
 router.get("/:id", controller.getAuthor);
 router.post("/", authenticate, requireAdmin, upload.single("avatar"), controller.createAuthor);
 router.put("/:id", authenticate, requireAdmin, upload.single("avatar"), controller.updateAuthor);
